344bec44c9f17b9ff368d5dedc66044828dd55de
Ismile Hossain <iamismile.dev@gmail.com>
2021-11-22 09:50:10 +0600

    refactor(app): Apply globally scoped Pipe and Middleware

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,9 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
 
 describe('Authentication System', () => {
   let app: INestApplication;
@@ -14,21 +12,11 @@ describe('Authentication System', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    app.use(
-      cookieSession({
-        keys: ['asdfasfd'],
-      }),
-    );
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-      }),
-    );
     await app.init();
   });
 
   it('handles a signup request', () => {
-    const email = 'asde@mail.com';
+    const email = 'asde21@mail.com';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
